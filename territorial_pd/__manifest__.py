213fcd0f414d9c3a2625ffaa37fc4cb322917c42
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2024-09-27 08:27:39 -0400

    [FIX] Fixed the 'C8102 (manifest-required-key) Missing required key "license" in manifest file'

--- territorial_pd/__manifest__.py
+++ territorial_pd/__manifest__.py
@@ -23,6 +23,7 @@
     "author": "Sinapsys Global SA, MASTERCORE SAS, Odoo Community Association (OCA)",
     "website": "http://sinapsys.global",
     "category": "Localization",
+    "license": "LGPL-3",
     "version": "0.1",
     "depends": ["base", "contacts"],
     "data": [
