eadc5aaacfb0948a3091862d969e7a4082546ffa
Andyclodofy <anderson.armeya@gmail.com>
2025-09-20 00:23:33 -0400

    [ADD] res_currency_rate_provider_BCV: scraping BCV

---
+++ res_currency_rate_provider_BCV/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Currency rate provider: BCV",
+    "summary": """Currency rate provider for BCV (Banco Central de Venezuela)""",
+    "version": "18.0.1.1.2",
+    "category": "Financial Management/Configuration",
+    "author": "Anderson Armeya, Odoo Community Association (OCA)",
+    "maintainers": ["andyengit"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["currency_rate_update"],
+    "data": [],
+}
---
+++ res_currency_rate_provider_BCV/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds the Central Bank of Venezuela (BCV) as a currency exchange provider.
+
+Currencies available: Dólar (USD), Euro (EUR), Yuan (CNY), Rublo (RUB), Lira (TRY)
